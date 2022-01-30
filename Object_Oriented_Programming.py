5f58d89d0788638ea40aede961f8502dca4c36a2
Kellie Thomsen <98286186+kelliethomsen@users.noreply.github.com>
2022-01-30 16:10:41 +1000

    30/01/22 4:10pm

--- Object_Oriented_Programming.py
+++ Object_Oriented_Programming.py
@@ -1,1 +1,24 @@
+class Car:
 
+    def __init__(self, make, model, year, colour):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.colour = colour
+   
+    def drive(self):
+        print("This "+self.model+" is driving")
+    
+    def stop(self):
+        print("This "+self.model+" is stopped")
+
+car_1 = Car("Chevy","Corvette",2021,"blue")
+car_2 = Car("Ford","Mustang",2022,"red")
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.colour)
+
+car_1.drive()
+car_2.stop()
