dee72ebb67405e3e55196111524da612b7dc9336
Kellie Thomsen <98286186+kelliethomsen@users.noreply.github.com>
2022-01-30 10:30:57 +1000

    30/01/22 10:30am

--- String_Format.py
+++ String_Format.py
@@ -2,4 +2,23 @@
 
 number = 1000
 
-print("The number pi is {:.3f")
+print("The number pi is {:.3f}".format(number))
+print("The number is {:,}".format(number))
+print("The number is {:b}".format(number))
+print("The number is {:o}".format(number))
+print("The number is {:X}".format(number))
+print("The number is {:E}".format(number))
+
+#another one...
+
+animal = "cow"
+item = "moon"
+
+print("The {} jumped over the {}".format("cow","moon"))
+#or
+print("The {} jumped over the {}".format(animal,item))
+#or 
+print("The {1} jumped over the {0}".format(animal,item)) #positional argument
+#or
+text = "The {} junped over the {}"
+print(text.format(animal,item))
